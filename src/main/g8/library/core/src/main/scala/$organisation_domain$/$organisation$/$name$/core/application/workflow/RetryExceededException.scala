406cc346fdc008654f979fa12988281dcfada3dc
Carl Pulley <carlp@cakesolutions.net>
2017-07-18 11:03:24 +0100

    Scalafmt fix

--- src/main/g8/library/core/src/main/scala/$organisation_domain$/$organisation$/$name$/core/application/workflow/RetryExceededException.scala
+++ src/main/g8/library/core/src/main/scala/$organisation_domain$/$organisation$/$name$/core/application/workflow/RetryExceededException.scala
@@ -4,7 +4,7 @@ package $organisation_domain$.$organisation$.$name$.core.application.workflow
   * TODO:
   */
 final class RetryExceededException(message: String)
-  extends Exception(message) {
+    extends Exception(message) {
 
   /** @see java.lang.Exception */
   override def toString: String = {
